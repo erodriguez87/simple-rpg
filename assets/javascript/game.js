56c480d1e059f38c75d2067bcf2174a958f81b77
Eddie Rodriguez <erodriguez87@gmail.com>
2018-03-04 19:49:43 -0500

    holy crap it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ var playerAp =0;
 var defender = "";
 var defenderHealth = 0 ;
 var defenderCap = 0;
+var defeatedCount = 0;
 
 var scoreDiv = $(".score");
 var target = Math.floor(Math.random() * 90)+19; console.log ('target', target);
@@ -15,6 +16,7 @@ var bullpenBool2 = true;
 
 var wins = 0;
 var losses = 0;
+
 
 //Logic that determines what happens with clicks on the character images
 $('.char').click(function() {
@@ -28,7 +30,8 @@ $('.char').click(function() {
     ($(this).attr('src',""));
     ($(this).attr('alt',""));
     ($('.hero').attr('src',playerImg));
-    ($('.instructions').text('Pick a defender'))
+    ($('.instructions').text('Pick a defender'));
+    ($('.heroHP').text('Health: ' + playerHealth + ' Attack Power ' + playerAp));
     console.log('player name post click', player,'health', playerHealth, 'attack power', playerAp, playerImg);
     // winCondition();
 
@@ -43,13 +46,33 @@ $('.char').click(function() {
     ($(this).attr('alt',""));
     ($('.defender').attr('src',defenderImg));
     ($('.instructions').text('Good Luck! Click Attack to take him down'))
+    $('.attackBtn').prop('disabled', false);
+    ($('.defenderHP').text('Health: ' + defenderHealth + ' Counter Power ' + defenderCap));    
     console.log('defender name post click', defender,'health', defenderHealth, 'counter', defenderCap, defenderImg);
   } 
 });
 
-// function healthcheck();{
+var heroAni = $('.hero');
 
-// }
+function winReset(){
+  ($('.aragorn').attr('src','assets/images/aragorn.png'));
+  ($('.gandalf').attr('src','assets/images/gandalf.png'));
+  ($('.legolas').attr('src','assets/images/legolas.png'));
+  ($('.gimli').attr('src','assets/images/gimli.png'));
+  ($('.defeated').attr('src',''));
+  ($('.hero').attr('src',''));
+  ($('.defender').attr('src',''));
+  ($('.instructions').text('welcome back challenger, chose the same hero or try a different adventure'));
+  bullpenBool = true;
+  bullpenBool2 =  true;
+  player = "";
+  playerHealth = 0;
+  playerAp =0;
+  defender = "";
+  defenderHealth = 0 ;
+  defenderCap = 0;
+  defeatedCount = 0;
+ }
 
 $('.attackBtn').click(function() {
   // player health is above zero and defender health is above zero
@@ -65,16 +88,30 @@ $('.attackBtn').click(function() {
         return;
       }
     playerAp += playerAp;
+    $('.heroHP').text('Health: ' + playerHealth + ' Player Attack Power ' + playerAp);
+    $('.defenderHP').text('Health: ' + defenderHealth + ' Counter Power ' + defenderCap);  
     console.log('player hp ',playerHealth, 'defender hp', defenderHealth, 'player AP',playerAp);
   // player health is still above zero but defender is dead
   } else if((playerHealth >= 0) &&(defenderHealth <= 0 )){
-    ($('.instructions').text('you took down', defender, 'please choose a new enemy'));
+    ($('.instructions').text('you took down that guy down, please choose a new enemy'));
     bullpenBool2 = true;
+    ($('.heroHP').text('Health: ' + playerHealth + ' Player Attack Power ' + playerAp));    
     ($('.defender').attr('src',''));
     ($('.defeated').attr('src',defenderImg));
+    defeatedCount++;
+    $('.attackBtn').prop('disabled', true);
+      if (defeatedCount === 3) {
+        wins++;
+        alert('you win!!');
+        winReset();
+      }
+      $('.heroHP').text('Health: ' + playerHealth + ' Player Attack Power ' + playerAp);
+      $('.defenderHP').text('Health: ' + defenderHealth + ' Counter Power ' + defenderCap);  
   // player is dead... lost game
   } else if ((playerHealth <= 0) &&(defenderHealth >= 0)){
     alert('you lose, try again')
+    losses++;
+    winReset();
   }
     
 });
